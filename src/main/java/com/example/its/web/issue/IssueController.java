e9d468923ac7ee0d5e9fce7b73085fffe0d091e3
jongsu824 <jongsu824@gmail.com>
2023-10-02 23:01:00 +0900

    IntelliJではじめる Spring Boot

---
+++ src/main/java/com/example/its/web/issue/IssueController.java
@@ -0,0 +1,24 @@
+package com.example.its.web.issue;
+
+import com.example.its.domain.issue.IssueEntity;
+import com.example.its.domain.issue.IssueService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class IssueController {
+
+    private  final IssueService issueService;
+    // GET /issues
+    @GetMapping("/issues")
+    public String showList(Model model) {
+        model.addAttribute("issueList", issueService.findAll());
+        return "issues/list";
+    }
+
+}
