f247a325bf99503ac26afe67375254f59aa1d844
jongsu824 <jongsu824@gmail.com>
2023-10-03 19:29:54 +0900

    IntelliJではじめるSpring Boot

--- src/main/java/com/example/its/domain/issue/IssueEntity.java
+++ src/main/java/com/example/its/domain/issue/IssueEntity.java
@@ -1,2 +1,13 @@
-package com.example.its.domain.issue;public class IssueEntity {
+package com.example.its.domain.issue;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class IssueEntity {
+    private long id;
+    private String summary;
+    private String description;
+
 }
--- src/main/java/com/example/its/web/issue/IssueController.java
+++ src/main/java/com/example/its/web/issue/IssueController.java
@@ -1,2 +1,24 @@
-package com.example.its.web.issue;public class IssueController {
+package com.example.its.web.issue;
+
+import com.example.its.domain.issue.IssueService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.ui.Model;
+import com.example.its.domain.issue.IssueEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class IssueController {
+
+    private final IssueService issueService;
+
+    // GET /issues
+    @GetMapping("/issues")
+    public String showList(Model model){
+        model.addAttribute("issueList", issueService.findAll());
+        return "issues/list";
+    }
 }
